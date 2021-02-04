a1b35fba06d5f0054605b0b2f000d181a8e46835
nmydt <62681228+nmydt@users.noreply.github.com>
2021-02-04 22:30:12 +0800

    Update ips.py

--- Spider/ips.py
+++ Spider/ips.py
@@ -1,8 +1,5 @@
-
-import requests,random,json
-from lxml import etree
-import requests
-
+import subprocess as sp
+import requests,json,random,re
 User_Agent = [
 	'Mozilla/5.0 CK={} (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
 	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
@@ -27,102 +24,136 @@ User_Agent = [
 
 headers = {}
 headers['User-Agent'] = random.choice(User_Agent)
+ip_pools=[] #未过滤的ip池
+new_pools=[]#已过滤的ip池
 
-temp_set = set()
-def get_66ip():
-    def parse_page(url):
-        r = requests.get(url,headers=headers)
-        if r.status_code == 200:
+"""
+函数说明:获取IP代理
 
-            html = etree.HTML(r.text)
-        #     print(etree.tostring(html,encoding='utf-8').decode('utf-8'))
-            trs = html.xpath('//div[@align="center"]/table//tr')
-            for tr in trs[1:]:
-        #             print(etree.tostring(tr,encoding='utf-8').decode('utf-8'))
-                    ip = tr.xpath('.//td[1]/text()')[0]
-                    port = tr.xpath('.//td[2]/text()')[0]
-                    ip_port = ip+":"+port
-                    temp_set.add(ip_port)
-    def main():
-        for i in range(1,34):
-            url = "http://www.66ip.cn/areaindex_%d/1.html" %i
-            parse_page(url)
+"""
 
-
-    if __name__ == '__main__':
-        main()
 def pro():
+    
     url = "http://proxylist.fatezero.org/proxy.list"
     r = requests.get(url,headers=headers)
-    if r.status_code == 200:
-        lists = r.text.split('\n')
-        for i in lists[0:50]:
-            try:
-                li = json.loads(i,strict=False)
-                if str(li['anonymity']) == 'high_anonymous' and str(li['type']) == 'http':
-
-                    ip_port = str(li['host'])+":"+str(li['port'])
-                    temp_set.add(ip_port)
-            except:
-                continue
-def kuai():
-    def parse_page(url):
-        r = requests.get(url,headers=headers)
-        html = etree.HTML(r.text)
-        trs = html.xpath('//tbody//tr')    
-        for tr in trs:
-            ip = tr.xpath('./td[1]/text()')[0]
-            port = tr.xpath('./td[2]/text()')[0]
-            ip_port = ip+":"+port
-            if ip_port in temp_set:
-                temp_set.remove(ip_port)
-            temp_set.add(ip_port)
-            
-
-    def main():
-        for i in range(1,30):
-            url = "https://www.kuaidaili.com/free/inha/%d" %i
-            parse_page(url)
-
-
-    if __name__ == '__main__':
-        main()
-def test_proxy():
-    for ip_port in temp_set.copy():
-        proxy = {
-            'http':ip_port
-        }
+    lists = r.text.split('\n')
+    for i in lists:
         try:
-            r = requests.get('http://www.baidu.com',headers=headers,proxies=proxy,timeout=5)
-            print(r.status_code)
-            if r.status_code != 200:
-                
+            li = json.loads(i,strict=False)
+            if str(li['anonymity']) == 'high_anonymous' and str(li['type']) == 'http':
 
-                temp_set.remove(ip_port)
+                ip_port = str(li['host'])+":"+str(li['port'])
 
-            else:
-                print("true:{}".format(ip_port))
+                ip_pools.append(ip_port)
         except:
-            temp_set.remove(ip_port)
+            continue
+
+
+"""
+函数说明:检查代理IP的连通性
+Parameters:
+	ip - 代理的ip地址
+	lose_time - 匹配丢包数
+	waste_time - 匹配平均时间
+Returns:
+	average_time - 代理ip平均耗时
+Modify:
+	2017-05-27
+"""
+
+def check_ip(ip, lose_time, waste_time):
+    #命令 -n 要发送的回显请求数 -w 等待每次回复的超时时间(毫秒)
+    cmd = "ping -w 3 %s"
+    #执行命令
+    p = sp.Popen(cmd % ip, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True) 
+    #获得返回结果并解码
+    out = p.stdout.read().decode("gbk")
+    #丢包数
+    lose_time = lose_time.findall(out)
+    #当匹配到丢失包信息失败,默认为三次请求全部丢包,丢包数lose赋值为3
+    if len(lose_time) == 0:
+        lose = 3
+    else:
+        lose = int(lose_time[0])
+    #如果丢包数目大于2个,则认为连接超时,返回平均耗时1000ms
+    if lose > 25:
+        #返回False
+        return 1000
+    #如果丢包数目小于等于2个,获取平均耗时的时间
+    else:
+        #平均时间
+        average = waste_time.findall(out)
+        #当匹配耗时时间信息失败,默认三次请求严重超时,返回平均好使1000ms
+        if len(average) == 0:
+            return 1000
+        else:
+            #
+            average_time = float(average[0])
+            #返回平均耗时
+            return average_time
+
+"""
+函数说明:进一步检查代理IP的可用性
+Parameters:
+	ip_port - 代理ip
+
+"""
+def check_ip2(ip_port):
+    proxy = {
+        'http':ip_port
+    }
+    try:
+        r = requests.get('http://www.baidu.com',headers=headers,proxies=proxy,timeout=5)
+        if r.status_code != 200:
             print("faild:{}".format(ip_port))
-if __name__ == '__main__': 
-    print("***正在抓取http代理ip***")
-    # get_66ip()
-    # kuai()
-    pro()
-    print("***success***")
-    ago = len(temp_set)
-    print("***正在检测http代理ip可用性***")
-    test_proxy()
-    present = len(temp_set)
-    temp = list(temp_set)
+            return -1
+        else:
+            return 200
+    except:
+        print("faild:{}".format(ip_port))
+        return -1
+"""
+函数说明:初始化正则表达式
+Parameters:
+	无
+Returns:
+	lose_time - 匹配丢包数
+	waste_time - 匹配平均时间
+
+"""
+def initpattern():
+    #匹配丢包数
+    lose_time = re.compile(u"\s(\d+)%", re.IGNORECASE)
+    #匹配平均时间
+    waste_time = re.compile(u"/(\d.*?)/", re.IGNORECASE)
+    return lose_time, waste_time
+
+"""
+函数说明:保存代理
+
+"""
+def save_proxy():
+    f = open('./ip_pool/ips.html','w')
+    f.write(str(new_pools))
+
+if __name__ == '__main__':
+    #初始化正则表达式
+    lose_time, waste_time = initpattern()
+    #获取IP代理
     
-    f = open('./ip_pool/pool.html','w',encoding='utf-8')
-    for t in temp:
-
-        f.write('\''+t+'\','+'\n')
-    f.close
-    print("***success***")
-    print({'共抓取ip数':ago,'可用ip':present})
-    print("***请在当前目录下pool.html文件查看成功抓取的可用ip***")
-
+    pro()
+    #如果平均时间超过200ms重新选取ip
+    for proxy in ip_pools:
+        split_proxy = proxy.split(':')
+        #获取IP
+        ip = split_proxy[0]
+        
+        #检查ip
+        average_time = check_ip(ip, lose_time, waste_time)
+        print(average_time)
+        if average_time < 200:
+            if check_ip2(proxy)==200:
+                print(proxy+"验证成功")
+                new_pools.append(proxy)
+    
+    save_proxy()
